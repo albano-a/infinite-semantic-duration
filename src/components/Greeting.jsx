8eb47f9f6e88a7b0294dfcf02dd0155ce5f8553d
[Andre Albano] <110417140+albano-a@users.noreply.github.com>
2024-05-12 00:26:38 -0300

    feat: Update landing page with navigation links and improve formatting

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -4,12 +4,21 @@ export default function Greeting({ messages }) {
   const randomMessage = () =>
     messages[Math.floor(Math.random() * messages.length)];
 
-  const [greeting, setGreeting] = useState(messages[0]);
+  const [greeting, setGreeting] = useState(randomMessage);
 
   return (
     <div>
-      <h3>{greeting}! Thank you for visiting!</h3>
-      <button onClick={() => setGreeting(randomMessage())}>New Greeting</button>
+      <h2 className="is-size-4 has-text-centered">{greeting}</h2>
+      <div className="columns is-centered">
+        <div className="column is-narrow is-full-mobile">
+          <button
+            className="button is-rounded is-dark"
+            onClick={() => setGreeting(randomMessage())}
+          >
+            New Quote
+          </button>
+        </div>
+      </div>
     </div>
   );
 }

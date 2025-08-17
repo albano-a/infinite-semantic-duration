12014837f4b5dcdeb6e7b2b49d0b9405aa9233e3
André Albano <andre_barros@id.uff.br>
2025-08-17 00:51:46 -0300

    Svelte added to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import preact from "@astrojs/preact";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://infinite-semantic-duration.netlify.app/",
-  integrations: [preact(), mdx(), sitemap()],
+  integrations: [preact(), mdx(), sitemap(), svelte()],
 });
